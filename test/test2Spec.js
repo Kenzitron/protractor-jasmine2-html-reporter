a489a90fbd2c8db63eb779f102f1c595a056fabd
Kenzitron <alejandro@kenzilab.com>
2015-05-12 09:18:03 +0200

    Test Fixed

--- test/test2Spec.js
+++ test/test2Spec.js
@@ -2,22 +2,22 @@ describe('SPEC 2', function() {
     it('2 should add a todo', function() {
         browser.get('http://www.angularjs.org');
 
-        element(by.model('todoText')).sendKeys('write a protractor test');
+        element(by.model('todoList.todoText')).sendKeys('write a protractor test');
         element(by.css('[value="add"]')).click();
 
-        var todoList = element.all(by.repeater('todo in todos'));
+        var todoList = element.all(by.repeater('todo in todoList.todos'));
         expect(todoList.count()).toEqual(3);
         expect(todoList.count()).toEqual(6); //Failure
         expect(todoList.get(2).getText()).toEqual('write a protractor test');
     });
 
-    it('2 segundo it', function() {
+    it('2 second it', function() {
         browser.get('http://www.angularjs.org');
 
-        element(by.model('todoText')).sendKeys('write a protractor test');
+        element(by.model('todoList.todoText')).sendKeys('write a protractor test');
         element(by.css('[value="add"]')).click();
 
-        var todoList = element.all(by.repeater('todo in todos'));
+        var todoList = element.all(by.repeater('todo in todoList.todos'));
         expect(todoList.count()).toEqual(3);
         expect(todoList.count()).toEqual(3);
         expect(todoList.count()).toEqual(3);
